617ce685d8c50d9a028da7cfd6781338288f9293
DylDev <110776467+DylDevs@users.noreply.github.com>
2024-03-31 13:41:34 -0400

    Fix issue opening installer

--- menu.bat
+++ menu.bat
@@ -4,4 +4,6 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 echo Administrator mode enabled.
 echo Enabling powershell scripts for this console...
-powershell -command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process;echo 'Done, opening script...';.\InstallerSources\installer.ps1"
+powershell -command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+echo 'Done, opening script...'
+Powershell.exe -File  ./installer.ps1
